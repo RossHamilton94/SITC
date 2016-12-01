2fb666033654425c77679744f50fc0ea4162b9c3
J.P.Barnes U1357474 <U1357474@unimail.hud.ac.uk>
2016-12-01 16:26:44 +0000

    Added spawn gizmos and the new level geometry

---
+++ Assets/Spawnpoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawnpoint : MonoBehaviour
+{
+    public float debugSize = 2.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(this.transform.position, Vector3.one * debugSize);
+    }
+}
