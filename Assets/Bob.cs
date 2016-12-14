13a1b8d9ee49f536dccd934126c7b5f45d331406
Syphonx <contact@syphonx.me>
2016-12-14 00:49:10 +0000

    Added some stuff Added some textures Added explosions Fixed rope Added then removed fire > N.B i changed the menu to run as boss only and disabled quite a bit of ui that was getting in my way

---
+++ Assets/Bob.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bob : MonoBehaviour {
+
+    float originalY;
+    public float floatStrength = 1; 
+
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            originalY + ((float)Mathf.Sin(Time.time) * floatStrength),
+            transform.position.z);
+    }
+}
---
+++ Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySelf : MonoBehaviour {
+
+    public float lifetime;
+
+    void Awake()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
