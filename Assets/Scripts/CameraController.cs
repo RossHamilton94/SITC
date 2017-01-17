98032cff104fc968118a880d625b4187cbd4b50b
J.P.Barnes U1357474 <U1357474@unimail.hud.ac.uk>
2017-01-17 11:12:07 +0000

    added letterbox

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,32 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-	public float shakeIntensity = 0.5f;
-	public int shakes = 10;
-	public float speed = 1;
+    public float shakeIntensity = 0.5f;
+    public int shakes = 10;
+    public float speed = 1; 
 
-	// Use this for initialization
-	void Start () { 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-		
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
     /// <summary>
     /// OnGUI is called for rendering and handling GUI events.
     /// This function can be called multiple times per frame (one call per event).
     /// </summary>
     void OnGUI()
     { 
-        // if (GUI.Button(new Rect(10, 70, 100, 100), "SHAKE DAT ASS")) {
-			// Camera.main.GetComponent<CameraControl>().Shake(shakeIntensity, shakes, speed);
-        // }
     }
- 
+
     void OnEnable()
     {
         SlamController.OnSlam += ScreenShake;
@@ -37,9 +43,9 @@ public class CameraController : MonoBehaviour {
         SlamController.OnSlam -= ScreenShake;
     }
 
-	void ScreenShake()
-	{
-		Camera.main.GetComponent<CameraControl>().Shake(shakeIntensity, shakes, speed);
-	} 
+    void ScreenShake()
+    {
+        Camera.main.GetComponent<CameraControl>().Shake(shakeIntensity, shakes, speed);
+    }
 
 }
