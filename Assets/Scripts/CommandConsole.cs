55179f053578dff21f339947dce9e90c834f1a31
Steve <dragarth@gmail.com>
2017-01-19 19:27:21 +0000

    Added some commands for starting the game into the command console

--- Assets/Scripts/CommandConsole.cs
+++ Assets/Scripts/CommandConsole.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class CommandConsole : MonoBehaviour
 {
     public static CommandConsole instance = null;
 
     public GameObject commandPanel;
+    public MenuController mc;
 
     public InputField commandInput;
     public Text previousCommands;
@@ -44,39 +47,102 @@ public class CommandConsole : MonoBehaviour
         {
             active = true;
             commandPanel.SetActive(active);
+            commandInput.text = "";
+            EventSystem.current.SetSelectedGameObject(commandPanel);
+            EventSystem.current.SetSelectedGameObject(commandInput.gameObject);
+        }
+        if(mc == null && SceneManager.GetActiveScene().name == "Menu")
+        {
+            mc = GameObject.FindGameObjectWithTag("MenuController").GetComponent<MenuController>();
         }
     }
 
     public void CheckCommand()
     {
         string commandToCheck = commandInput.text.ToLower();
-        previousCommands.text = previousCommands.text + "\n";
+        if (!commandToCheck.Contains("`"))
+            previousCommands.text = previousCommands.text + "\n";
 
         if(commandToCheck == "" || commandToCheck == null)
         {
             previousCommands.text = previousCommands.text + "Please enter a command...";
         }
+        else if (commandToCheck.Contains("`"))
+        {
+
+        }
         else if(commandToCheck.Contains("time"))
         {
-            if(commandToCheck.Contains("stop"))
+            if(commandToCheck.Contains("stop") && !commandToCheck.Contains("start"))
             {
                 Time.timeScale = 0;
+                previousCommands.text = previousCommands.text + commandToCheck;
             }
-            else if (commandToCheck.Contains("start"))
+            else if (commandToCheck.Contains("start") && !commandToCheck.Contains("stop"))
             {
                 Time.timeScale = 1;
+                previousCommands.text = previousCommands.text + commandToCheck;
+            }
+            else
+            {
+                previousCommands.text = previousCommands.text + "Did not recognise: '" + commandToCheck + "'.";
             }
         }
-        else if (commandToCheck.Contains("player charge"))
+        else if (commandToCheck.Contains("player"))
         {
-            if (commandToCheck.Contains("0"))
+            if (commandToCheck.Contains("charge"))
             {
-                
+                if (commandToCheck.Contains("0"))
+                {
+
+                }
+                else if (commandToCheck.Contains("1"))
+                {
+
+                }
             }
-            else if (commandToCheck.Contains("1"))
+            
+        }
+        else if(commandToCheck.Contains("scene"))
+        {
+            if (commandToCheck.Contains("reset"))
             {
-                
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
+            else if (commandToCheck.Contains("load"))
+            {
+                if(commandToCheck.Contains("1"))
+                {
+                    SceneManager.LoadScene("Level_1");
+                }
+                else if(commandToCheck.Contains("menu"))
+                {
+                    SceneManager.LoadScene("Menu");
+                }
+            }
+        }
+        else if(commandToCheck.Contains("start"))
+        {
+            // start-4-0-1-1
+            string tempCommandToCheck = commandToCheck;
+            string[] splitCommand = tempCommandToCheck.Split('-');
+            int noOfPlayers = 0;
+            int bossPlayer = 0;
+            int keyboardPlayer = 0;
+            int levelToLoad = 0;
+            int.TryParse(splitCommand[1], out noOfPlayers);
+            int.TryParse(splitCommand[2], out bossPlayer);
+            int.TryParse(splitCommand[3], out keyboardPlayer);
+            int.TryParse(splitCommand[4], out levelToLoad);
+            mc.currentNoOfPlayers = noOfPlayers;
+            mc.currentlyTheBoss = bossPlayer;
+            mc.currentKeyboardPlayer = keyboardPlayer;
+            mc.LoadLevel(levelToLoad);
+            previousCommands.text = previousCommands.text + "Loading level, Please wait.";
+        }
+        else
+        {
+            previousCommands.text = previousCommands.text + "Did not recognise: '" + commandToCheck + "'.";
         }
     }
 }
