2ef81148f63f07f920fc419e63a2452f57e6ba7b
J.P.Barnes U1357474 <U1357474@unimail.hud.ac.uk>
2017-02-02 11:41:19 +0000

    Fixed some lighting things

--- Assets/Scripts/Bob.cs
+++ Assets/Scripts/Bob.cs
@@ -1,20 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bob : MonoBehaviour {
+public class Bob : MonoBehaviour
+{
 
     float originalY;
-    public float floatStrength = 1; 
+    public float floatStrength = 1;
+
+    public bool startOffset = true;
+    private float floatDelay = 1.0f;
 
     void Start()
     {
         this.originalY = this.transform.position.y;
+        if (startOffset)
+        {
+            floatDelay = Random.Range(0.5f, 2.0f);
+            StartCoroutine(BobUpdate());
+        }
+    }
+    IEnumerator BobUpdate()
+    {
+        yield return new WaitForSeconds(floatDelay);
+        startOffset = false;
     }
 
     void Update()
     {
-        transform.position = new Vector3(transform.position.x,
-            originalY + ((float)Mathf.Sin(Time.time) * floatStrength),
-            transform.position.z);
+        if (!startOffset)
+        {
+            transform.position = new Vector3(transform.position.x,
+                originalY + ((float)Mathf.Sin(Time.time) * floatStrength),
+                transform.position.z);
+        }
     }
 }
