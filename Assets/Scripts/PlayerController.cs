d36787a859a8bbd27d35b3929de576b8abb9cd7f
Steve <dragarth@gmail.com>
2017-02-15 19:56:30 +0000

    Just about finished with the boss and player prefabs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -351,6 +351,10 @@ public class PlayerController : Entity
             GetComponent<Rigidbody>().velocity = new Vector2(
                 -groundState.wallDirection() * (speed * mod_factor) * (wall_push * mod_factor),
                 GetComponent<Rigidbody>().velocity.y); //Add force negative to wall direction (with speed reduction)
+        if (groundState.isGround())
+            anim.SetBool("Grounded", true);
+        else
+            anim.SetBool("Grounded", false);
 
         input.y = 0;
     }
@@ -448,7 +452,7 @@ public class PlayerController : Entity
             transform.position = spawnPos;
             em.clonesRemaining--;
             em.ui.UpdateCloneNumber(em.clonesRemaining);
-            StartCoroutine(MoveOnSpawn());
+            //StartCoroutine(MoveOnSpawn());
         }
         else
         {
